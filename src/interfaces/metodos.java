3972237b0916b51fb737107006fcf59fddb17045
LN710Q <LN710Q@DEILABO1-PC31>
2018-06-11 13:00:26 -0600

    Se creo la BD

---
+++ src/interfaces/metodos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.ArrayList;
+import static jdk.nashorn.internal.codegen.types.Type.generic;
+
+
+/**
+ *
+ * @author LN710Q
+ */
+public interface metodos <Generic>{
+    public boolean create(Generic g);
+    public boolean delete (Object key);
+    public boolean update (Generic c);
+   
+    public Generic read(Object key);
+    public ArrayList<Generic> readAll();
+    
+}
